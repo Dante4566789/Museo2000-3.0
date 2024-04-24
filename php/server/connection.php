8ba282b34d4878983135f75c060baa1c355784ab
Riccardo <107932703+Dante4566789@users.noreply.github.com>
2024-04-24 15:19:30 +0000

    create new file server

---
+++ php/server/connection.php
@@ -0,0 +1,16 @@
+<?php
+//pagina di connessione al database 
+
+
+$conn = new mysqli('localhost','palladoriccardo','nq83zEmY2A68','my_palladoriccardo');
+
+if($conn -> connect_error){
+    die('connessione fallita : ' .$conn -> connect_error);
+
+}else{
+    echo "dc";
+}
+
+
+
+?>
