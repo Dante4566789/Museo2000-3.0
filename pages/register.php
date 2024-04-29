5ad9fcd14b84da161d2e580fea59490fda8620b3
Riccardo <107932703+Dante4566789@users.noreply.github.com>
2024-04-29 19:24:50 +0200

    Add files via upload

---
+++ pages/register.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Museo2000</title>
+  <link rel="stylesheet" href="../global.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <link rel="icon" type="image/x-icon" href="../public/favicon.ico">
+</head>
+
+<body>
+
+  <!--HTML-->
+  <div class="Login">
+    <div class="wrapper">
+      <form action="login.php" method="POST">
+        <h1>Login</h1>
+        <div class="input-box">
+          <input type="text" name="email" placeholder="Email" required>
+          <i class='bx bxs-user'></i>
+        </div>
+        <div class="input-box">
+          <input type="password" name="password" placeholder="Password" required>
+          <i class='bx bxs-lock-alt'></i>
+        </div>
+        <div class="remember-forgot">
+          <label><input name="checkbox" type="checkbox">Ricordati di me</label>
+        </div>
+        <button type="submit" class="btn">Login</button>
+        <div class="register-link">
+          <p>Vai alla home <a href="home.php">Home</a></p>
+        </div>
+      </form>
+    </div>
+
+  </div>
+
+
+  <!--PHP-->
+  <?php
+  
+  ?>
+
+</body>
+
+</html>
